bfc433d8d8533f297353c1fe5e8d1f2b79cd131c
manikantakaredla <luckyha0637k@gmail.com>
2026-06-15 21:26:43 +0530

    changes 2

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,4 +1,9 @@
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+
 const nodemailer = require("nodemailer");
+
+
 
 const sendEmail = async (to, subject, text, html = null) => {
   try {
@@ -14,19 +19,20 @@ const sendEmail = async (to, subject, text, html = null) => {
     console.log("SMTP_PORT:", process.env.SMTP_PORT || 587);
     console.log("=========================================");
 
-    const transporter = nodemailer.createTransport({
+   const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
   requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000
+  tls: {
+    family: 4,
+  },
 });
+
     console.log("🔍 Verifying SMTP connection...");
 
     await transporter.verify();
