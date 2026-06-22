40249ee615b393f6a612c1f5585bf8cc918cff98
manikantakaredla <luckyha0637k@gmail.com>
2026-06-22 22:59:24 +0530

    frontend changes

--- campx-client/src/pages/admin/UserManagement.jsx
+++ campx-client/src/pages/admin/UserManagement.jsx
@@ -337,10 +337,7 @@ const UserManagement = () => {
           )}
         </div>
       )}
-
-      </div>
-
-      <div className=\"mb-6 flex flex-col md:flex-row gap-4 bg-white p-4 rounded-xl shadow-sm border border-gray-100\">
+      <div className="mb-6 flex flex-col md:flex-row gap-4 bg-white p-4 rounded-xl shadow-sm border border-gray-100">
         <div className="flex-1 relative">
           <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
           <input
